784ff26763cba8ceebd8e582dd8411550b24a6fa
Arthur Brites <arth.brites@gmail.com>
2022-04-15 18:35:37 -0300

    Added Navbar and added Feed Container

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
 import NavBar from "./NavBar";
-
+import Feed from "./Feed";
+import "../styles/MainContainer.css";
 export default function Home() {
   return (
     <>
       <NavBar />
+      <div className="main">
+        <div></div>
+        <Feed />
+        <div></div>
+      </div>
     </>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Navbar, Container } from "react-bootstrap";
+import profilePic from "../images/default pfp.png";
+import logo from "../images/logo192.png";
+import { Navbar, NavDropdown, Image } from "react-bootstrap";
 
 export default function NavBar() {
   return (
-    <Navbar bg="light">
-      <Container>
-        <Navbar.Brand href="#home">(insira uma logo dahora)</Navbar.Brand>
-      </Container>
+    <Navbar bg="light" style={{ paddingLeft: "40px", paddingRight: "40px" }}>
+      <Navbar.Brand href="#home">
+        <Image src={logo} height="40px" width="40px"></Image>
+      </Navbar.Brand>
+
+      <NavDropdown id="navbarScrollingDropdown" className="ms-auto">
+        <NavDropdown.Item href="#action3">Perfil</NavDropdown.Item>
+        <NavDropdown.Item href="#action4">Configurações</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action5">Logout</NavDropdown.Item>
+      </NavDropdown>
+      <Image src={profilePic} height="40px" width="40px" rounded="y"></Image>
     </Navbar>
   );
 }
