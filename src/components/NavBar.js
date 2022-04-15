7ff4355138ceddbc5dbf1b10bc1bb02d50d4a08b
Arthur Brites <arth.brites@gmail.com>
2022-04-15 13:22:44 -0300

    Initial Commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navbar, Container } from "react-bootstrap";
+
+export default function NavBar() {
+  return (
+    <Navbar bg="light">
+      <Container>
+        <Navbar.Brand href="#home">(insira uma logo dahora)</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+}
