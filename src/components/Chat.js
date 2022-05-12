03152945ac23b3bac703b3abe993b2c579527385
Allan Lima dos Santos <Lima.allancontato@gmail.com>
2022-05-12 19:33:27 -0300

    Construção do chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,17 +1,31 @@
 import React from "react";
-import { Widget } from 'react-chat-widget';
+import { useEffect } from 'react';
+import { Widget, addResponseMessage  } from 'react-chat-widget';
 
 import 'react-chat-widget/lib/styles.css';
 
+import logo from './logo.jpeg';
+
+
 export default function Chat() {
+    useEffect(() => {
+        addResponseMessage('Welcome to this awesome chat!');
+      }, []);
+
     const handleNewUserMessage = (newMessage) => {
         console.log(`New message incoming! ${newMessage}`);
+        // Now send the message throught the backend API
     };
 
     return (
         <>
             <div className="App">
-                <Widget />
+                <Widget         
+                    handleNewUserMessage={handleNewUserMessage}
+                    profileAvatar={logo}
+                    title="My new awesome title"
+                    subtitle="And my cool subtitle"
+                />
             </div>
         </>
     );
