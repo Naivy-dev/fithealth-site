d078ef9f343c2dd9c00bfbe94b171a88910ddf74
Arthur Brites <arth.brites@gmail.com>
2022-05-19 00:53:32 -0300

    fix profileName

--- src/components/ProfileContainer.js
+++ src/components/ProfileContainer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card, ListGroup } from "react-bootstrap";
 import pfp from "../images/default pfp.png";
-const desc = `Olá, me chamo Gabriel, sou apaixonado por nutrição e estou disposto a ajudar a todos.
+const desc = `Olá, me chamo João, sou apaixonado por nutrição e estou disposto a ajudar a todos.
 Desde pequeno tive interesse pela área pois meus pais já atuavam na área voltada para saúde, e me interessei por essa influência.
 Meu objetivo é ser um nutricionista de alta qualidade disposto a oferecer o melhor tipo de solução que eu conseguir para os problemas que aparecer.`;
 export default function ProfileContainer() {
