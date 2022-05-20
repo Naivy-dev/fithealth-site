24ff3294379be5beee02ef16a47f7ecd719b9ea2
Arthur Brites <arth.brites@gmail.com>
2022-05-20 00:34:01 -0300

    Added a lot of stuff and i don't have time to comment it

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,18 +3,24 @@ import NavBar from "./NavBar";
 import Feed from "./Feed";
 import ProfileContainer from "./ProfileContainer";
 import "../styles/MainContainer.css";
+import LeftSideMenu from "./LeftSideMenu";
+import RightSideMenu from "./RightSideMenu";
 export default function Home() {
   const [mode, setMode] = useState("feed");
   return (
     <>
       <NavBar setMode={setMode} />
       <div className="main">
-        <div></div>
+        <div>
+          <LeftSideMenu />
+        </div>
         <div className="mainContainer">
           {mode === "feed" && <Feed />}
           {mode === "profile" && <ProfileContainer />}
         </div>
-        <div></div>
+        <div>
+          <RightSideMenu />
+        </div>
       </div>
     </>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import profilePic from "../images/default pfp.png";
 import logo from "../images/logobrabaSemFundo.png";
+import searchIcon from "../images/search-icon.png";
 import { Navbar, NavDropdown, Image } from "react-bootstrap";
 
 export default function NavBar(props) {
@@ -20,12 +21,24 @@ export default function NavBar(props) {
         paddingLeft: "40px",
         paddingRight: "100px",
         backgroundColor: "#f7fffc",
+        boxShadow: "0 -3px 10px",
       }}
     >
       <Navbar.Brand onClick={goToFeed}>
-        <Image src={logo} height="100%" width="100px"></Image>
+        <Image src={logo} height="100%" width="72px"></Image>
       </Navbar.Brand>
-
+      <div className="wrap">
+        <div className="search">
+          <input type="text" className="searchTerm" placeholder="Pesquisar" />
+          <button type="submit" className="searchButton">
+            <img
+              alt="Pesquisar"
+              src={searchIcon}
+              style={{ height: "70%", paddingRight: 3 }}
+            />
+          </button>
+        </div>
+      </div>
       <NavDropdown id="navbarScrollingDropdown" className="ms-auto">
         <NavDropdown.Item onClick={goToProfile}>Perfil</NavDropdown.Item>
         <NavDropdown.Item>Configurações</NavDropdown.Item>
